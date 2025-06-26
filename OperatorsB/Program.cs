45681fc715028a7eb1d5d24466fd0c6ff1d5dec7
kumar6646 <sai.penusoft@gmail.com>
2025-06-26 11:20:06 +0530

    Add project files.

---
+++ OperatorsB/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsB
+{
+    /*
+     * What is operator:
+     * It perform operation between two operands.
+     * for ex:
+     * c=a+b;
+     * here a and b are called operands 
+     * where "+" is an operator.
+     * 
+     * Types of Operator:
+     * 1)Arithmetic
+     * 2)Relational
+     * 3)Assignment
+     * 4)Logical
+     * 5)Conditional or ternary
+     * 6)Increment and Decrement
+     * 7)Bit wise 
+     * 
+     * Arithmetic Operator is used to perform
+     * arithmetic operations like addition,substraction,
+     * multiplication,division and modulus.
+     * 
+     * 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Sub();
+            Mul();
+            Div();
+            Modulus();
+        }
+        private static void Add()
+        {
+            Console.WriteLine("Enter a value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("sum of {0} and {1} is {2}", a, b, c);
+
+        }
+        private static void Sub()
+        {
+            Console.WriteLine("Enter a value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value");
+            int b = int.Parse(Console.ReadLine());
+            int c = a - b;
+            Console.WriteLine("diff of {0} and {1} is {2}", a, b, c);
+
+        }
+        private static void Mul()
+        {
+            Console.WriteLine("Enter a value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value");
+            int b = int.Parse(Console.ReadLine());
+            int c = a * b;
+            Console.WriteLine("mul of {0} and {1} is {2}", a, b, c);
+
+        }
+        private static void Div()
+        {
+            Console.WriteLine("Enter a value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value");
+            int b = int.Parse(Console.ReadLine());
+            int c = a / b;
+            Console.WriteLine("div of {0} and {1} is {2}", a, b, c);
+
+        }
+        private static void Modulus()
+        {
+            Console.WriteLine("Enter a value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value");
+            int b = int.Parse(Console.ReadLine());
+            int c = a % b;
+            Console.WriteLine("Mod of {0} and {1} is {2}", a, b, c);
+
+        }
+    }
+}
